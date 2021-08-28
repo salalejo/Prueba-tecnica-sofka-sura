1c4c99dca021718191cefd3622ea2f7d17e1db77
Alejandro Salazar Garcia <alejandro.salazarg@sofka.com.co>
2021-08-27 23:18:15 -0500

    se añade version de java para heroku

--- src/main/java/com/co/constructores/pruebatecnica/controller/ConstructionRequestController.java
+++ src/main/java/com/co/constructores/pruebatecnica/controller/ConstructionRequestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api")
 public class ConstructionRequestController {
 
